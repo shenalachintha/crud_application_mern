c3b99e21cfe418885ea9a0be52073b1db3635122
shenalachintha <achinthas666@gmail.com>
2025-12-18 21:04:11 +0530

    database connect

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express= require('express')
+const mongoose= require('mongoose')
 const app=express();
 
-app.listen(3000,()=>{
+
+app.get('/',(req,res)=>{
+    res.send('Hello server is running')
+})
+mongoose.connect('mongodb+srv://admin:20020209@applicationdb.qi79asq.mongodb.net/Crud_App?appName=ApplicationDb')
+.then(()=>{
+    console.log('connected to database')
+    app.listen(3000,()=>{
     console.log('sever running on port 3000')
+})
+}).catch((err)=>{
+    console.log('error connecting to database',err)
+
 })
