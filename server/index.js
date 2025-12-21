525fcf0f18808e3bbbe8d46215dc098b7fd67d4c
shenalachintha <achinthas666@gmail.com>
2025-12-21 08:14:44 +0530

    crud api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,67 @@
 const express= require('express')
 const mongoose= require('mongoose')
+const Product=require('./product.model')
 const app=express();
+
+
+app.use(express.json());
 
 
 app.get('/',(req,res)=>{
     res.send('Hello server is running')
 })
-app.post('/api/products',(req,res)=>{
-    console.log(req.body)
-    res.send(req.body)
+app.get('/api/products',async (req,res)=>{
+    try{
+        const products= await Product.find({});
+        res.status(200).json(products);
+
+    }catch(err){
+        res.status(500).json({message:'internal server error'})
+    }
+})
+app.get('/api/products/:id',async (req,res)=>{
+    try{
+        const product= await Product.findById(req.params.id);       
+        if(!product){
+            return res.status(404).json({message:'product not found'})
+        }   
+        res.status(200).json(product);
+
+    }catch(err){
+        res.status(500).json({message:'internal server error'})
+    }   
+})
+app.post('/api/products',async (req,res)=>{
+   try{
+    const product = await Product.create(req.body) 
+    res.status(201).json(product);
+   }catch(err){
+    res.status(500).json({message:'internal server error'})
+}})
+app.put('/api/products/:id',async (req,res)=>{
+    try{
+        const product=await Product.findByIdAndUpdate(req.params.id,req.body);
+            if(!product){
+                return res.status(404).json({message:'product not found'})
+        }
+        const updateProduct=await Product.findById(req.params.id);
+        res.status(200).json(updateProduct);
+    }
+    catch(err){
+        res.status(500).json({message:'internal server error'})
+    }
+})
+app.delete('/api/products/:id',async (req,res)=>{
+    try{
+        const product=await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+            return res.status(404).json({message:'product not found'})
+        }
+        res.status(200).json({message:'product deleted successfully'});
+
+    }catch(err){
+        res.status(500).json({message:'internal server error'})
+    }
 })
 mongoose.connect('mongodb+srv://admin:20020209@applicationdb.qi79asq.mongodb.net/Crud_App?appName=ApplicationDb')
 .then(()=>{
